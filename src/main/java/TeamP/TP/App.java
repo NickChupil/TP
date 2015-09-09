48fef44501edc4cc05c3140261c379ca67a6d254
NickChupil <niklaus.nc@gmail.com>
2015-09-09 21:18:32 +0300

    Json Pars

--- src/main/java/TeamP/TP/App.java
+++ src/main/java/TeamP/TP/App.java
@@ -1,13 +1,60 @@
 package TeamP.TP;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.io.File;
+import java.io.IOException;
+import org.codehaus.jackson.JsonFactory;
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.JsonParser;
+import org.codehaus.jackson.JsonToken;
+import org.codehaus.jackson.map.JsonMappingException;
+
+public class App {
+	public static void main(String[] args) {
+
+		try {
+
+			JsonFactory jfactory = new JsonFactory();
+			JsonParser jParser = jfactory.createJsonParser(new File("" + "src/main/java/TeamP/TP/students.json"));
+
+			while (jParser.nextToken() != JsonToken.END_ARRAY) {
+
+				String fieldname = jParser.getCurrentName();
+				if ("surname".equals(fieldname)) {
+
+					jParser.nextToken();
+					System.out.println(jParser.getText());
+				}
+
+				if ("age".equals(fieldname)) {
+
+					jParser.nextToken();
+					System.out.println(jParser.getIntValue());
+				}
+
+				if ("course".equals(fieldname)) {
+
+					jParser.nextToken();
+					System.out.println(jParser.getText());
+					System.out.println("\n");
+				}
+
+			}
+			jParser.close();
+
+		} catch (JsonGenerationException e) {
+
+			e.printStackTrace();
+
+		} catch (JsonMappingException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		}
+
+	}
+
 }
